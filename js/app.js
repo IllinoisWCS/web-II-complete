0d049377d7a91656a343d333f33ed1ba7616dff9
Emily Chao <elchao96@live.com>
2016-08-24 19:09:04 -0500

    added some more explanations

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
-angular.module('jeopardyApp', ['ngRoute'])
+angular.module('jeopardyApp', ['ngRoute', 'ngSanitize'])
 
 .config(['$routeProvider', function($routeProvider) {
+    /**
+     * Routes provide a way for us to navigate from one web view to
+     * another. Each view has a different controller. Think of the
+     * view as what you see on the web page and the controller controls
+     * how that view got generated / how it changes as users interact
+     * with the view.
+     */
     $routeProvider
         .when('/', {
             templateUrl: 'partials/home.html',
@@ -19,6 +26,13 @@ angular.module('jeopardyApp', ['ngRoute'])
         });
 }])
 
+/**
+ * A service serves as the main hub for all data requests (aka, the model).
+ * We want to have a centralized place where we can get the question data
+ * and this service might expand if we decide to do different types of data
+ * requests in the future. But for now, this is the one-stop-shop to getting
+ * the question data.
+ */
 .service('QuestionService', ['$http', function($http) {
     return {
         getQuestionData: function() {
@@ -27,11 +41,29 @@ angular.module('jeopardyApp', ['ngRoute'])
     };
 }])
 
+/**
+ * Each controller has a $scope object. This syntax always injects
+ * this $scope object into each controller. How $scope works can get
+ * quite complicated as more complexity is added to an Angular app,
+ * but for our purposes, each controller has its own $scope. Which means,
+ * any kind of variable state or changes live within this controller and no
+ * where else. It is possible to share different variables amongst different
+ * controllers, but we will discuss that later.
+ */
 .controller('MainController', ['$scope', function($scope) {
     $scope.customMessage = 'Let\'s play Jeopardy.';
 }])
 
+/**
+ * A controller can have variables as well as functions attached to its $scope.
+ * These can control how the view is laid out and what changes happen.
+ * We can also inject services into a controller so that the controller can have
+ * access and use them. In our case, we would like to get the question data, so
+ * our controller has access to the QuestionService, which provides one method
+ * to access the data.
+ */
 .controller('RandomController', ['$scope', 'QuestionService', function($scope, QuestionService) {
+    $scope.currentScore = 0;
     /*
      * Returns a random integer between min (inclusive) and max (exclusive)
      */
@@ -39,6 +71,13 @@ angular.module('jeopardyApp', ['ngRoute'])
         min = Math.ceil(min);
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min)) + min;
+    };
+
+    $scope.generateQuestion = function() {
+        var randomInteger = getRandomInteger(0, $scope.allQuestions.length);
+        $scope.randomQuestion = $scope.allQuestions[randomInteger];
+        $scope.alreadySeenQuestion = false;
+        $scope.answerIsShowing = false;
     };
 
     $scope.initialize = function() {
@@ -52,9 +91,13 @@ angular.module('jeopardyApp', ['ngRoute'])
             });
     };
 
-    $scope.generateQuestion = function() {
-        var randomInteger = getRandomInteger(0, $scope.allQuestions.length);
-        $scope.randomQuestion = $scope.allQuestions[randomInteger];
+    var parseNumericValue = function(value) {
+        return parseInt(value.substring(1));
+    };
+
+    $scope.addToScore = function() {
+        $scope.currentScore += parseNumericValue($scope.randomQuestion.value);
+        $scope.alreadySeenQuestion = true;
     };
 }])
 
